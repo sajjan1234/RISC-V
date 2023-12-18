7b185a2ae3c295c8f0fce6e9f55db975af572ee3
sajjan <kumarsajjan404@gmail.com>
2023-12-18 15:07:42 -0800

    Initial Commit

--- Execute_pipeline.v
+++ Execute_pipeline.v
@@ -1,6 +1,7 @@
 `include "ALU.v"
 
-module execute_cycle(rs1_E, rs2_E, AluControl_E, Result_E, WriteData_E);
+module execute_cycle(rs1_E, rs2_E, AluControl_E, Result_E, WriteData_E, RegWrite_E, ImmSrc_E, ALUSrc_E, MemWrite_E,
+                    ResultSrc_E, Branch_E);
 
 //declaration of ports
 input [31:0]rs1_E, rs2_E;
