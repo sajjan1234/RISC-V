6a71fd0401aca723be93cab177288d87d869f24a
sajjan <kumarsajjan404@gmail.com>
2023-12-19 09:09:59 -0800

    Initial commit

--- Execute_pipeline.v
+++ Execute_pipeline.v
@@ -1,11 +1,12 @@
 `include "ALU.v"
 `include "Mux.v"
 
-module execute_cycle(RD1_E, RD2_E, AluControl_E, WriteData_E, RegWrite_E, ImmExt_E, ALUSrc_E, MemWrite_E,
+module execute_cycle(RD1_E, RD2_E, AluControl_E, RegWrite_E, ImmExt_E, ALUSrc_E, MemWrite_E,
                     ResultSrc_E, Branch_E, RegWrite_M, ResultSrc_M, MemWrite_M,WriteData_M, AluResult_M, );
 
 //declaration of ports
-input [31:0]RD1_E, RD2_E;
+input RegWrite_E, ALUSrc_E, MemWrite_E, ResultSrc_E, Branch_E;
+input [31:0]RD1_E, RD2_E, ImmExt_E;
 input [2:0]ALUControl_E;
 
 output [31:0]AluResult_M, WriteData_M;
@@ -15,4 +16,25 @@ output RegWrite_M, ResultSrc_M, MemWrite_M;
 wire [31:0]AluResult_i;
 
 //Declaration of Registers
+reg [31:0]AluResult_M_r, WriteData_M_r;
+reg RegWrite_M_r, ResultSrc_M_r, MemWrite_M_r;
+
+//Instantiation of modules
+ALU alu(
+    .A(),
+    .B(),
+    .Result(),
+    .ALUControl()
+);
+
+Mux mux(
+    .a(),
+    .b(),
+    .s(),
+    .c()
+);
+
+
+
+
 
